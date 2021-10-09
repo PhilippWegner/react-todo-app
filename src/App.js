725a30b4801e1804e4f298c20f819b46fff3d8e9
pwegner <wegner@ial.ruhr>
2021-10-09 21:47:23 +0200

    toggleTodos

--- src/App.js
+++ src/App.js
@@ -21,6 +21,27 @@ function App() {
       event.target.value = "";
     }
   };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((t) => {
+        if (t.id === id) {
+          return { ...t, done: !t.done };
+          // if (t.done === false) {
+          //   return { ...t, done: true };
+          // }
+          // else {
+          //   return { ...t, done: false };
+          // }
+        }
+        return t;
+      })
+    );
+  };
+
+  const activeTodos = todos.filter(({ done }) => !done);
+  const doneTodos = todos.filter(({ done }) => done);
+
   return (
     <div className="App">
       <div className="HeaderContainer">
@@ -44,9 +65,13 @@ function App() {
 
         <div className="TodoListContainer">
           <h2>Zu erledigen:</h2>
-          {todos.map((t) => (
+          {activeTodos.map((t) => (
             <div key={t.id} className="TodoItemContainer">
-              <input type="checkbox"></input>
+              <input
+                checked={t.done}
+                onChange={() => toggleTodo(t.id)}
+                type="checkbox"
+              ></input>
               <p className="TodoItemText">{t.title}</p>
               <button
                 onClick={() => handleDeleteTodo(t.id)}
@@ -60,12 +85,22 @@ function App() {
 
         <div className="TodoListContainer">
           <h2>Erledigt:</h2>
-
-          <div className="TodoItemContainer">
-            <input type="checkbox"></input>
-            <p className="TodoItemText">Todo</p>
-            <button className="TodoItemDeleteButton">&#x2715;</button>
-          </div>
+          {doneTodos.map((t) => (
+            <div key={t.id} className="TodoItemContainer">
+              <input
+                checked={t.done}
+                onChange={() => toggleTodo(t.id)}
+                type="checkbox"
+              ></input>
+              <p className="TodoItemText">{t.title}</p>
+              <button
+                onClick={() => handleDeleteTodo(t.id)}
+                className="TodoItemDeleteButton"
+              >
+                &#x2715;
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
