4738bb16d6219bc1f221475af3f036356e5c6558
pwegner <wegner@ial.ruhr>
2021-10-09 17:50:35 +0200

    html+css skelett

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="HeaderContainer">
+        <div className="HeaderContent">
+          <img style={{ width: 120 }} src={logo} alt="React-Logo" />
+          <h1>React Todo</h1>
+        </div>
+        <div className="SearchInputContainer">
+          <input className="InputItem" placeholder="Suche" />
+        </div>
+      </div>
+
+      <div className="MainContainer">
+        <div className="TodoInputContainer">
+          <input placeholder="Hier Todo hinzufügen" className="InputItem" />
+        </div>
+
+        <div className="TodoListContainer">
+          <h2>Zu erledigen:</h2>
+
+          <div className="TodoItemContainer">
+            <input type="checkbox"></input>
+            <p className="TodoItemText">Todo</p>
+            <button className="TodoItemDeleteButton">&#x2715;</button>
+          </div>
+        </div>
+
+        <div className="TodoListContainer">
+          <h2>Erledigt:</h2>
+
+          <div className="TodoItemContainer">
+            <input type="checkbox"></input>
+            <p className="TodoItemText">Todo</p>
+            <button className="TodoItemDeleteButton">&#x2715;</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
