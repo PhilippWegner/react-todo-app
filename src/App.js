ef14969fc07e4878427080bfc253ded1cfeecd7e
pwegner <wegner@ial.ruhr>
2021-10-09 18:09:19 +0200

    addTodo + uuid()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { title: "Test Todo", id: uuid(), done: false },
+  ]);
+
+  const handleAddTodo = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        { title: event.target.value, id: uuid(), done: false },
+      ]);
+      event.target.value = "";
+    }
+  };
   return (
     <div className="App">
       <div className="HeaderContainer">
@@ -16,17 +31,22 @@ function App() {
 
       <div className="MainContainer">
         <div className="TodoInputContainer">
-          <input placeholder="Hier Todo hinzufügen" className="InputItem" />
+          <input
+            onKeyDown={handleAddTodo}
+            placeholder="Hier Todo hinzufügen"
+            className="InputItem"
+          />
         </div>
 
         <div className="TodoListContainer">
           <h2>Zu erledigen:</h2>
-
-          <div className="TodoItemContainer">
-            <input type="checkbox"></input>
-            <p className="TodoItemText">Todo</p>
-            <button className="TodoItemDeleteButton">&#x2715;</button>
-          </div>
+          {todos.map((t) => (
+            <div className="TodoItemContainer">
+              <input type="checkbox"></input>
+              <p className="TodoItemText">{t.title}</p>
+              <button className="TodoItemDeleteButton">&#x2715;</button>
+            </div>
+          ))}
         </div>
 
         <div className="TodoListContainer">
